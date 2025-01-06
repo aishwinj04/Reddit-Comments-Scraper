03af80c3746fd2c3ffbcdb2dee53f4592d6905f1
aishwinj04 <aishwin@gmail.com>
2025-01-06 00:31:41 -0500

    Get Post function

--- main.py
+++ main.py
@@ -9,36 +9,14 @@ user=os.getenv('r_username')
 passcode=os.getenv('r_password')
 
 
-# from reddit app 
-reddit = praw.Reddit(user_agent='My Reddit Bot', client_id=id, client_secret=secret, username=user, password=passcode)
+# initialize reddit instanece and get post 
+def get_post(post_link):
 
+    reddit = praw.Reddit(user_agent='My Reddit Bot', client_id=id, client_secret=secret, username=user, password=passcode)
 
-url = 'https://www.reddit.com/r/TorontoMetU/comments/18jxacd/anyone_know_some_of_the_easiest_table_a_lower/'
+    post = reddit.submission(url=post_link)
 
-post = reddit.submission(url=url)
-title = post.title
-body = post.selftext
+    return post
 
-print(post.title)
-
-# get all comments
-post.comments.replace_more(limit=None)
-
-data = []
-
-print(len(post.comments))
-for comment in post.comments.list():
-    data.append({
-        'User': comment.author,
-        'Comment': comment.body,
-        'Score': comment.score 
-    })
-
-
-to_df = df.DataFrame(data)
-print(to_df)
-    
-# now add to a csv file 
-to_df.to_csv('results.csv', index=False)
 
 
