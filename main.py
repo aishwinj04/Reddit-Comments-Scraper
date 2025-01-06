7759f6f6dcf5160117a87014d960bb1416248909
aishwinj04 <aishwin@gmail.com>
2025-01-05 23:28:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ passcode=os.getenv('r_password')
 
 
 # from reddit app 
-reddit = praw.Reedit(userAgent=True, client_id=id,
-                     client_secret=secret, username=user, password=passcode)
+reddit = praw.Reddit(user_agent='My Reddit Bot', client_id=id, client_secret=secret, username=user, password=passcode)
 
 
+url = 'https://www.reddit.com/r/vscode/comments/1htzfkm/try_it_these_6_vscode_settings_will_bring_your/'
+
+post = reddit.submission(url)
+print(post)
